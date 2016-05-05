057d55a23b0fe17c03c893d40349c2fab711c644
Kai KANG <myfavouritekk@gmail.com>
2016-05-06 01:36:52 +0800

    Add option to disable NMS when showing video detection results. Fix bugs in show_video_matfile_detection.py

--- tools/visual/show_video_matfile_detection.py
+++ tools/visual/show_video_matfile_detection.py
@@ -18,7 +18,9 @@ if __name__ == '__main__':
     parser.add_argument('det_root')
     parser.add_argument('--cls', choices=imagenet_vdet_class_idx.keys())
     parser.add_argument('--save_dir', default=None)
-    parser.add_argument('--top_k', default=10)
+    parser.add_argument('--top_k', default=10, type=int)
+    parser.add_argument('--no_nms', action='store_false', dest='nms')
+    parser.set_defaults(nms=True)
     args = parser.parse_args()
 
     vid_proto = proto_load(args.vid_file)
@@ -32,10 +34,20 @@ if __name__ == '__main__':
         det = sio.loadmat(det_file)
         frame_idx = frame['frame']
         img = imread(frame_path_at(vid_proto, frame_idx))
-        boxes = det['boxes'][:,cls_index,:]
-        scores = det['zs'][:,cls_index]
-        keep = nms(np.hstack((boxes,scores[:,np.newaxis])), 0.3)
-        det_img = add_bbox(img, [boxes[i,:] for i in keep], [scores[i] for i in keep])
+        boxes = det['boxes'][:,cls_index,:].astype('single')
+        scores = det['zs'][:,cls_index].astype('single')
+        if args.nms:
+            keep = nms(np.hstack((boxes,scores[:,np.newaxis])), 0.3)
+        else:
+            keep = range(len(boxes))
+        kept_boxes = [boxes[i,:] for i in keep]
+        kept_scores = [scores[i] for i in keep]
+        top_idx = np.argsort(np.asarray(kept_scores))[::-1]
+        top_boxes = [kept_boxes[top_idx[i]] for i in \
+            xrange(min(args.top_k, len(kept_boxes)))]
+        top_scores = [kept_scores[top_idx[i]] for i in \
+            xrange(min(args.top_k, len(kept_boxes)))]
+        det_img = add_bbox(img, top_boxes, top_scores)
         cv2.imshow('detection', det_img)
         if cv2.waitKey(0) == ord('q'):
             cv2.destroyAllWindows()
