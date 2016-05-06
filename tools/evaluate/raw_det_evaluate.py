2d22cd8b0c779648ac36874a4c0720270227fa90
Kai KANG <myfavouritekk@gmail.com>
2016-05-06 23:47:00 +0800

    Fix NMS bug in evaluation

--- tools/evaluate/raw_det_evaluate.py
+++ tools/evaluate/raw_det_evaluate.py
@@ -17,41 +17,56 @@ def image_name_at_frame(vid_proto, frame_idx):
         if frame['frame'] == frame_idx:
             return os.path.join(vid_name, os.path.splitext(frame['path'])[0])
 
-def single_vid_raw_det_eval(input_list):
+def single_vid_raw_det_eval(input_list, thresh=0.00, max_per_image=100, nms_thres=0.3,
+        num_classes=31):
     vid_file, det_folder, image_set = input_list
     vid_proto = proto_load(vid_file)
     print vid_proto['video']
 
-    # dets are [frame_idx, class_index, cls_score, x1, y1, x2, y2]
-    dets = []
+    det_strings = []
     for frame in vid_proto['frames']:
         frame_name = os.path.splitext(frame['path'])[0]
         det_file = os.path.join(det_folder, "{}.mat".format(frame_name))
         local_idx = frame['frame']
+        image_name = image_name_at_frame(vid_proto, local_idx)
+        frame_idx = image_set[image_name]
         try:
             det = sio.loadmat(det_file)
-        except ValueError:
-            print "det_file {} has been corrupted.".format(det_file)
+        except Exception as error:
+            print "Error {}: det_file {}.".format(error, det_file)
             continue
         boxes = det['boxes']
         scores = det['zs']
-        image_name = image_name_at_frame(vid_proto, local_idx)
-        frame_idx = image_set[image_name]
-        cur_dets = []
-        for reg_boxes, cls_scores in zip(boxes, scores):
-            for cls_index, (cls_bbox, cls_score) in \
-                enumerate(zip(reg_boxes, cls_scores)):
-                # skip background
-                if cls_index == 0: continue
-                cur_dets.append([int(frame_idx), cls_index,
-                    float(cls_score), cls_bbox.tolist()])
-        nms_boxes = [det[-1]+[det[2],] for det in cur_dets]
-        keep = nms(np.asarray(nms_boxes).astype('float32'), args.nms_thres)
-        for i in keep:
-            dets.append(cur_dets[i])
+        cur_boxes = [[] for _ in xrange(num_classes)]
+        for j in xrange(1, num_classes):
+            inds = np.where(scores[:,j] > thresh)[0]
+            if len(inds) == 0: continue
+            cls_scores = scores[inds, j]
+            cls_boxes = boxes[inds, j, :]
+            cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
+                .astype(np.float32, copy=False)
+            keep = nms(cls_dets, nms_thres)
+            cls_dets = cls_dets[keep, :]
+            cur_boxes[j] = cls_dets
 
-    dets.sort(key=lambda x:(x[0], x[1], -x[2]))
-    return dets
+        # Limit to max_per_image detections *over all classes*
+        if max_per_image > 0:
+            image_scores = np.hstack([cur_boxes[j][:, -1] \
+                for j in xrange(1, num_classes)])
+            if len(image_scores) > max_per_image:
+                image_thresh = np.sort(image_scores)[-max_per_image]
+                for j in xrange(1, num_classes):
+                    keep = np.where(cur_boxes[j][:, -1] >= image_thresh)[0]
+                    cur_boxes[j] = cur_boxes[j][keep, :]
+
+        for class_index, cls_dets in enumerate(cur_boxes):
+            if class_index == 0: continue
+            for dets in cls_dets:
+                det_strings.append('{} {} {:.06f} {:.2f} {:.2f} {:.2f} {:.2f}\n'.format(
+                    frame_idx, class_index, dets[-1],
+                    dets[0], dets[1], dets[2], dets[3]))
+
+    return det_strings
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -68,7 +83,7 @@ if __name__ == '__main__':
         image_set = dict([line.strip().split() for line in f.readlines()])
     fp = open(args.save_file, 'w')
 
-    vidfiles = glob.glob(os.path.join(args.vid_root, '*.vid'))
+    vidfiles = sorted(glob.glob(os.path.join(args.vid_root, '*.vid')))
     det_folders = [os.path.join(args.raw_det_root,
         '{}'.format(os.path.splitext(os.path.basename(vid_file))[0])) \
             for vid_file in vidfiles]
@@ -78,13 +93,12 @@ if __name__ == '__main__':
 
     print "Evaluating {} dets...".format(len(input_list))
     pool = mp.Pool(args.pool_size)
-    dets = pool.map(single_vid_raw_det_eval, input_list)
+    det_strings = pool.map(single_vid_raw_det_eval, input_list)
+    # det_strings = map(single_vid_raw_det_eval, input_list)
 
     # flatten
-    kept_dets = [det for vid_det in dets for det in vid_det]
     print "Writing to {}...".format(args.save_file)
-    for frame_idx, class_index, score, bbox in kept_dets:
-        fp.write('{} {} {:.6f} {:.2f} {:.2f} {:.2f} {:.2f}\n'.format(
-            frame_idx, class_index, score,
-            bbox[0], bbox[1], bbox[2], bbox[3]))
+    for vid_strings in det_strings:
+        for string in vid_strings:
+            fp.write(string)
 
