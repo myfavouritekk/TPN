c2cde62d59f5c2403d118a8587008558c1725b64
Kai KANG <myfavouritekk@gmail.com>
2016-05-08 20:04:54 +0800

    Add regression propagation.

--- src/tpn/propagate.py
+++ src/tpn/propagate.py
@@ -60,3 +60,94 @@ def naive_box_regression(net_rpn, net_no_rpn, vid_proto,
                '{:d} object proposals').format(timer.total_time, boxes.shape[0])
     track_proto['tracks'] = tracks
     return track_proto
+
+
+def _box_proto_to_track(box_proto, max_frame, length):
+    # generate empty tracks according to box proto
+    tracks = []
+    for box in box_proto['boxes']:
+        track = []
+        for i in xrange(length):
+            if i == 0:
+                track_box = {
+                    "frame": box['frame'] + i,
+                    "roi": box['bbox'],
+                    "anchor": i
+                }
+            else:
+                track_box = {
+                    "frame": box['frame'] + i,
+                    "anchor": i
+                }
+            track.append(track_box)
+        tracks.append(track)
+    return tracks
+
+def _cur_rois(tracks, frame_id):
+    rois = []
+    index = []
+    for track_id, track in enumerate(tracks):
+        for box in track:
+            if box['frame'] == frame_id:
+                rois.append(box['roi'])
+                index.append(track_id)
+                break
+    return rois, index
+
+def _update_track(tracks, pred_boxes, track_index, frame_id):
+    for i, pred_box in zip(track_index, pred_boxes):
+        for box in tracks[i]:
+            if box['frame'] == frame_id:
+                box['bbox'] = pred_box.tolist()
+            if box['frame'] == frame_id + 1:
+                box['roi'] = pred_box.tolist()
+                break
+
+def roi_propagation(vid_proto, box_proto, net, scheme='max', length=None):
+    track_proto = {}
+    track_proto['video'] = vid_proto['video']
+    track_proto['method'] = 'roi_propagation'
+    max_frame = vid_proto['frames'][-1]['frame']
+    if not length: length = max_frame
+    tracks = _box_proto_to_track(box_proto, max_frame, length)
+    batch_size = 1024
+
+    for idx, frame in enumerate(vid_proto['frames'], start=1):
+        # Load the demo image
+        image_name = frame_path_at(vid_proto, frame['frame'])
+        im = imread(image_name)
+
+        # Detect all object classes and regress object bounds
+        # extract rois on the current frame
+        rois, track_index = _cur_rois(tracks, frame['frame'])
+        # print "Frame {}: {} proposals".format(frame['frame'], len(rois))
+        timer = Timer()
+        timer.tic()
+        # scores: n x c, boxes: n x (c x 4)
+        scores = []
+        boxes = []
+        for roi_batch in np.split(np.asarray(rois), range(0, len(rois), batch_size)[1:]):
+            s_batch, b_batch = im_detect(net, im, np.asarray(roi_batch))
+            scores.append(s_batch)
+            boxes.append(b_batch)
+        scores = np.concatenate(scores, axis=0)
+        boxes = np.concatenate(boxes, axis=0)
+        boxes = boxes.reshape((boxes.shape[0], -1, 4))
+
+        if scheme is 'mean':
+            # use mean regressions as predictios
+            pred_boxes = np.mean(boxes, axis=1)
+        elif scheme is 'max':
+            # use the regressions of the class with the maximum probability
+            # excluding __background__ class
+            max_cls = scores[:,1:].argmax(axis=1) + 1
+            pred_boxes = boxes[np.arange(len(boxes)), max_cls, :]
+        elif scheme is 'weighted':
+            # use class specific regression as predictions
+            pred_boxes = np.sum(boxes * scores[:,:,np.newaxis], axis=1) / np.sum(scores, axis=1, keepdims=True)
+        _update_track(tracks, pred_boxes, track_index, frame['frame'])
+        timer.toc()
+        print ('Detection took {:.3f}s for '
+               '{:d} object proposals').format(timer.total_time, len(rois))
+    track_proto['tracks'] = tracks
+    return track_proto
---
+++ tools/propagate/regression_propagation.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import os, sys, cv2
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import init
+import caffe
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import argparse
+import cPickle
+
+from vdetlib.utils.protocol import proto_load, proto_dump, frame_path_at
+from vdetlib.vdet.dataset import imagenet_vdet_classes, imagenet_det_200_class_idx
+from tpn.propagate import roi_propagation
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Regression propagation.')
+    parser.add_argument('vid_file')
+    parser.add_argument('box_file')
+    parser.add_argument('save_file')
+    parser.add_argument('--job', dest='job_id', help='Job slot id. GPU id + 1. [1]',
+                        default=1, type=int)
+    parser.add_argument('--def', dest='def_file', help='Network defination file.')
+    parser.add_argument('--param', help='Network parameter file.')
+    parser.add_argument('--scheme', help='Propagation scheme. [weighted]',
+                        choices=['max', 'mean', 'weighted'], default='weighted')
+    parser.add_argument('--length', type=int, default=9,
+                        help='Propagation length. [9]')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    # cfg.TEST.HAS_RPN = False  # Use RPN for proposals
+
+    args = parse_args()
+
+    caffe.set_mode_gpu()
+    caffe.set_device(args.job_id - 1)
+
+    net = caffe.Net(args.def_file, args.param, caffe.TEST)
+
+    print '\n\nLoaded network {:s}'.format(args.def_file)
+
+    vid_proto = proto_load(args.vid_file)
+    box_proto = proto_load(args.box_file)
+
+    track_proto = roi_propagation(vid_proto, box_proto, net, scheme=args.scheme,
+        length=args.length)
+
+    proto_dump(track_proto, args.save_file)
