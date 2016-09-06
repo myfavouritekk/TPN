08e91e41fe4cbea56a95ec13d810208d02dbf2da
Kai KANG <myfavouritekk@gmail.com>
2016-09-06 14:37:33 +0800

    Add support for mat v7.3 for box proposals.

--- tools/data/box_proto_from_proposals.py
+++ tools/data/box_proto_from_proposals.py
@@ -2,10 +2,8 @@
 
 import os, sys
 import os.path as osp
-import pdb
 import argparse
 import scipy.io as sio
-import h5py
 this_dir = osp.dirname(__file__)
 sys.path.insert(1, osp.join(this_dir, '../../external/'))
 from vdetlib.utils.protocol import proto_dump, path_to_index, proto_load, annot_boxes_at_frame
@@ -20,23 +18,39 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('proposal_file')
     parser.add_argument('vid_root')
-    parser.add_argument('annot_root')
     parser.add_argument('save_root')
+    parser.add_argument('--annot_root', type=str, default=None)
     args = parser.parse_args()
 
     if not os.path.isdir(args.save_root):
         os.makedirs(args.save_root)
 
-    mat_file = sio.loadmat(args.proposal_file)
-    image_names = mat_file['images']
-    all_boxes = mat_file['boxes']
+    h5 = False
+    try:
+        mat_file = sio.loadmat(args.proposal_file)
+        image_names = mat_file['images']
+        all_boxes = mat_file['boxes']
+    except NotImplementedError:
+        import h5py
+        h5 = True
+        mat_file = h5py.File(args.proposal_file, 'r')
+        image_names = mat_file.get('images')[0]
+        all_boxes = mat_file.get('boxes')[0]
     cur_vid_name = None
-    for [[image_name]], [boxes] in zip(image_names, all_boxes):
+    for image_name, boxes in zip(image_names, all_boxes):
+        if not h5:
+            image_name = image_name[0][0]
+            boxes = boxes[0]
+        else:
+            image_name = ''.join(map(unichr, mat_file[image_name].value))
+            boxes = mat_file[boxes].value.T
         parts = image_name.split('/')
         if len(parts) == 3:
             subset, video_name, frame_name = parts
         elif len(parts) == 4:
             __, subset, video_name, frame_name = parts
+        elif len(parts) == 2:
+            video_name, frame_name = parts
         else:
             raise ValueError('image name has {} components: {}'.format(
                 len(parts), image_name))
@@ -54,24 +68,36 @@ if __name__ == '__main__':
             # read vid_proto
             vid_proto = proto_load(
                 os.path.join(args.vid_root, cur_vid_name+'.vid'))
-            annot_proto = proto_load(
-                os.path.join(args.annot_root, cur_vid_name+'.annot'))
+            if args.annot_root:
+                annot_proto = proto_load(
+                    os.path.join(args.annot_root, cur_vid_name+'.annot'))
         # process boxes
         frame_idx = path_to_index(vid_proto, frame_name)
-        annot_boxes = annot_boxes_at_frame(annot_proto, frame_idx)
+        if args.annot_root:
+            annot_boxes = annot_boxes_at_frame(annot_proto, frame_idx)
         for box in boxes:
             bbox = box[0:4].tolist()
-            if len(annot_boxes) == 0:
-                overlaps = 0.
+            if args.annot_root:
+                # with GT
+                if len(annot_boxes) == 0:
+                    overlaps = 0.
+                else:
+                    overlaps = iou([bbox], annot_boxes)
+                box_proto['boxes'].append(
+                    {
+                        "frame": frame_idx,
+                        "bbox": box[0:4].tolist(),
+                        "positive": True if np.any(overlaps>=0.5) else False
+                    }
+                )
             else:
-                overlaps = iou([bbox], annot_boxes)
-            box_proto['boxes'].append(
-                {
-                    "frame": frame_idx,
-                    "bbox": box[0:4].tolist(),
-                    "positive": True if np.any(overlaps>=0.5) else False
-                }
-            )
+                # no GT
+                box_proto['boxes'].append(
+                    {
+                        "frame": frame_idx,
+                        "bbox": box[0:4].tolist(),
+                    }
+                )
     # save last proto
     save_if_not_exist(box_proto,
         os.path.join(args.save_root, cur_vid_name+'.box.gz'))
