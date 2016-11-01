b6de36cec53d26716ff340c0f42ecd36e3488e12
Kai KANG <myfavouritekk@gmail.com>
2016-11-01 23:59:20 +0800

    Add a script to convert track_proto to zip file for RNN training.

---
+++ tools/data/track_proto_to_zip.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+
+import sys
+import os.path as osp
+this_dir = osp.dirname(__file__)
+sys.path.insert(0, osp.join(this_dir, '../../src/'))
+sys.path.insert(0, osp.join(this_dir, '../../external/'))
+from vdetlib.utils.protocol import proto_load
+from tpn.data_io import save_track_proto_to_zip
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('proto_file')
+    parser.add_argument('save_zip')
+    args = parser.parse_args()
+
+    proto = proto_load(args.proto_file)
